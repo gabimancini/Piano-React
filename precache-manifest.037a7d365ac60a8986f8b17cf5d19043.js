4ea50f4a3a0acdc51764713a7a1ee850a54f011e
Gabriela <gabimancinir@gmail.com>
2020-09-11 15:46:13 -0400

    Primera versión

---
+++ precache-manifest.037a7d365ac60a8986f8b17cf5d19043.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a29686f0a3e8cacf2c3a91cb8c8c73",
+    "url": "/Piano-React/index.html"
+  },
+  {
+    "revision": "fc2c21b3aed04565bc89",
+    "url": "/Piano-React/static/css/main.7dabf456.chunk.css"
+  },
+  {
+    "revision": "f4d1096007ecae2c9486",
+    "url": "/Piano-React/static/js/2.9315facd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Piano-React/static/js/2.9315facd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2c21b3aed04565bc89",
+    "url": "/Piano-React/static/js/main.57d2868d.chunk.js"
+  },
+  {
+    "revision": "dbb88db0fc4cbbe15849",
+    "url": "/Piano-React/static/js/runtime-main.433594a3.js"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/Piano-React/static/media/do.18fd3f6f.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/Piano-React/static/media/fa.a787534a.wav"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/Piano-React/static/media/la.10af03f3.wav"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/Piano-React/static/media/mi.1f26c778.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/Piano-React/static/media/re.217b943a.wav"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/Piano-React/static/media/si.70924435.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/Piano-React/static/media/sol.785a33d2.wav"
+  }
+]);
